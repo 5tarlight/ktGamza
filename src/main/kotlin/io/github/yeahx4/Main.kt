6da46c77651c75b74f512eb603decd0eedf6ecad
5tarlight <developerstarlight@gmail.com>
2021-08-25 00:07:26 +0900

    readline and pause

--- src/main/kotlin/io/github/yeahx4/Main.kt
+++ src/main/kotlin/io/github/yeahx4/Main.kt
@@ -1,5 +1,12 @@
 package io.github.yeahx4
 
-fun main(args: Array<String>) {
+import io.github.yeahx4.util.Color
+import io.github.yeahx4.util.Console
 
+fun main(args: Array<String>) {
+    println("Welcome to ${Color.CYAN}Gamza${Color.RESET}.")
+    Console.pause()
+    Console.clear()
+
+    Console.readLine("이름 : ")
 }
--- src/main/kotlin/io/github/yeahx4/util/Console.kt
+++ src/main/kotlin/io/github/yeahx4/util/Console.kt
@@ -1,7 +1,33 @@
 package io.github.yeahx4.util
 
+import java.util.*
+
 class Console {
     companion object {
+        private fun isWindows(): Boolean {
+            return System.getProperty("os.name").lowercase().contains("windows")
+        }
 
+        fun clear() {
+            val cmd = when (isWindows()) {
+                true -> "cls"
+                false -> "clear"
+            }
+            Runtime.getRuntime().exec(cmd)
+        }
+
+        fun readLine(text: String?): String {
+            print(text)
+            with (Scanner(System.`in`)) {
+                return nextLine()
+            }
+        }
+
+        fun pause() {
+            println("${Color.RESET}계속하려면 아무키나 누르십시오.")
+            with (Scanner(System.`in`)) {
+                next()
+            }
+        }
     }
 }
